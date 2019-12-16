9ed90687650cbb3013c40faba1dc0571f89c3702
tomocy <tomocy.dev@gmail.com>
2019-12-16 20:09:37 +0900

    let _ClockState stop at a certain angle
    _ClockState stops at a certain angle for now to optimize its animation.

--- tomocy_clock/lib/tomocy_clock.dart
+++ tomocy_clock/lib/tomocy_clock.dart
@@ -103,116 +103,40 @@ class Clock extends StatefulWidget {
   _ClockState createState() => _ClockState();
 }
 
-class _ClockState extends State<Clock> with TickerProviderStateMixin {
-  DateTime _dateTime = DateTime.now();
-  Map<ClockType, AnimationController> _animationControllers = {};
-
-  @override
-  void initState() {
-    super.initState();
-
-    _updateAnimationControllers();
-  }
-
-  @override
-  void didUpdateWidget(Clock old) {
-    super.didUpdateWidget(old);
-
-    if (widget.is24Format != old.is24Format) {
-      _updateAnimationControllers();
-    }
-  }
-
-  void _updateAnimationControllers() => setState(() {
-        final ofSeconds = _dateTime.second / 60;
-        final ofMinutes = (_dateTime.minute + ofSeconds) / 60;
-        final ofHours = widget.is24Format
-            ? (_dateTime.hour + ofMinutes) / 24
-            : (_dateTime.hour % 12 + ofMinutes) / 12;
-
-        _animationControllers[ClockType.seconds] = AnimationController(
-          vsync: this,
-          duration: const Duration(seconds: 60),
-        )
-          ..forward(
-            from: ofSeconds,
-          )
-          ..repeat();
-        _animationControllers[ClockType.minutes] = AnimationController(
-          vsync: this,
-          duration: const Duration(minutes: 60),
-        )
-          ..forward(
-            from: ofMinutes,
-          )
-          ..repeat();
-        _animationControllers[ClockType.hours] = AnimationController(
-          vsync: this,
-          duration: widget.is24Format
-              ? const Duration(hours: 24)
-              : const Duration(hours: 12),
-        )
-          ..forward(
-            from: ofHours,
-          )
-          ..repeat();
-      });
-
-  @override
-  void dispose() {
-    _animationControllers.forEach((_, controller) => controller.dispose());
-
-    super.dispose();
-  }
+class _ClockState extends State<Clock> {
+  final Map<ClockType, double> _angles = {
+    ClockType.seconds: 45,
+    ClockType.minutes: 15,
+    ClockType.hours: 10,
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            final radius = constraints.maxHeight < constraints.maxWidth
-                ? constraints.maxHeight * 0.9 / 2
-                : constraints.maxWidth * 0.9 / 2;
-
-            return Stack(
-              alignment: AlignmentDirectional.topCenter,
-              children: <Widget>[
-                _secondsClockFace(
-                  radius: radius,
-                  center: _minutesClockFace(
-                    radius: radius * 3 / 4,
-                    center: _hoursClockFace(
-                      radius: radius / 2,
-                      indexStyle: 250 <= radius
-                          ? Theme.of(context).textTheme.display1
-                          : Theme.of(context).textTheme.headline,
-                    ),
-                    indexStyle: Theme.of(context).textTheme.subtitle,
-                  ),
-                  indexStyle: Theme.of(context).textTheme.caption,
-                ),
-                Container(
-                  width: 2,
-                  height: radius,
-                  color: Theme.of(context).accentColor,
-                ),
-              ],
-            );
-          },
+    final radius = 200.0;
+    return Center(
+      child: _secondsClockFace(
+        radius: radius,
+        center: _minutesClockFace(
+          radius: radius * 3 / 4,
+          center: _hoursClockFace(
+            radius: radius / 2,
+            indexStyle: Theme.of(context).textTheme.title,
+          ),
+          indexStyle: Theme.of(context).textTheme.subtitle,
         ),
+        indexStyle: Theme.of(context).textTheme.caption,
       ),
     );
   }
 
-  Widget _secondsClockFace({
+  ClockFace _secondsClockFace({
     double radius = 0,
     Color color = Colors.transparent,
     Widget center,
     TextStyle indexStyle,
   }) {
     return ClockFace(
-      turns: _animationControllers[ClockType.seconds],
+      angle: _angles[ClockType.seconds],
       radius: radius,
       center: center,
       indexes: List<int>.generate(60, (i) => (i + 15) % 60)
@@ -224,14 +148,14 @@ class _ClockState extends State<Clock> with TickerProviderStateMixin {
     );
   }
 
-  Widget _minutesClockFace({
+  ClockFace _minutesClockFace({
     double radius = 0,
     Color color = Colors.transparent,
     Widget center,
     TextStyle indexStyle,
   }) {
     return ClockFace(
-      turns: _animationControllers[ClockType.minutes],
+      angle: _angles[ClockType.minutes],
       radius: radius,
       center: center,
       indexes: List<int>.generate(60, (i) => (i + 15) % 60)
@@ -243,71 +167,24 @@ class _ClockState extends State<Clock> with TickerProviderStateMixin {
     );
   }
 
-  Widget _hoursClockFace({
+  ClockFace _hoursClockFace({
     double radius = 0,
     Color color = Colors.transparent,
     Widget center,
     TextStyle indexStyle,
   }) {
     return ClockFace(
-      turns: _animationControllers[ClockType.hours],
+      angle: _angles[ClockType.hours],
       radius: radius,
       center: center,
-      indexes: widget.is24Format
-          ? List<int>.generate(24, (i) => (i + 6) % 24)
+      indexes:
+          List<int>.generate(12, (i) => (i + 3) % 12 != 0 ? (i + 3) % 12 : 12)
               .map((i) => Text(
                     i % 3 == 0 ? '$i' : '・',
                     style: indexStyle,
                   ))
-              .toList()
-          : List<int>.generate(12, (i) => (i + 3) % 12)
-              .map(
-                  (i) => _dateTime.month == DateTime.april && _dateTime.day == 1
-                      ? _indexForAprilFool(
-                          i,
-                          style: indexStyle,
-                        )
-                      : Text(
-                          i % 3 == 0 ? i == 0 ? '12' : '$i' : '・',
-                          style: indexStyle,
-                        ))
               .toList(),
     );
-  }
-
-  Widget _indexForAprilFool(int i, {TextStyle style}) {
-    switch (i) {
-      case 0:
-        return Icon(
-          Icons.adb,
-          color: style.color,
-          size: style.fontSize,
-        );
-      case 3:
-        return Icon(
-          Icons.filter_3,
-          color: style.color,
-          size: style.fontSize,
-        );
-      case 6:
-        return Icon(
-          Icons.filter_6,
-          color: style.color,
-          size: style.fontSize,
-        );
-      case 9:
-        return Icon(
-          Icons.filter_9,
-          color: style.color,
-          size: style.fontSize,
-        );
-      default:
-        return Icon(
-          Icons.error_outline,
-          color: style.color,
-          size: style.fontSize,
-        );
-    }
   }
 }
 
@@ -316,16 +193,16 @@ enum ClockType { seconds, minutes, hours }
 class ClockFace extends StatelessWidget {
   const ClockFace({
     Key key,
-    @required this.turns,
+    @required this.angle,
     @required this.radius,
     this.color,
     this.center,
     this.indexes = const <Widget>[],
-  })  : assert(turns != null),
+  })  : assert(angle != null),
         assert(radius != null),
         super(key: key);
 
-  final Animation<double> turns;
+  final double angle;
   final double radius;
   final Color color;
   final Widget center;
@@ -333,42 +210,24 @@ class ClockFace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _rotating(
-      turns: turns,
-      inReverse: true,
+    return Transform.rotate(
+      angle: angle,
       child: CircleWithInnerEdges(
         radius: radius,
         color: color,
-        center: _rotating(
-          turns: turns,
+        center: Transform.rotate(
+          angle: -angle,
           child: center,
         ),
         innerEdges: indexes
-            .map((index) => _rotating(
-                  turns: turns,
-                  child: index,
-                ))
+            .map(
+              (index) => Transform.rotate(
+                angle: -angle,
+                child: index,
+              ),
+            )
             .toList(),
       ),
-    );
-  }
-
-  RotationTransition _rotating({
-    Animation<double> turns,
-    bool inReverse = false,
-    Widget child,
-  }) {
-    return RotationTransition(
-      turns: inReverse
-          ? Tween<double>(
-              begin: 1,
-              end: 0,
-            ).animate(turns)
-          : Tween<double>(
-              begin: 0,
-              end: 1,
-            ).animate(turns),
-      child: child,
     );
   }
 }
